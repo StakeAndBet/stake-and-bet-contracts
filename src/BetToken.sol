8e26467bf50ab2656717d1c71c0e01e03643e06e
idc <idc@nope>
2022-12-03 15:12:19 +0100

    Whitelist working & tests

--- src/BetToken.sol
+++ src/BetToken.sol
@@ -11,16 +11,28 @@ contract BetToken is ERC20, Ownable {
   event WhitelistAdded(address indexed account);
   event WhitelistRemoved(address indexed account);
 
-  constructor() ERC20("Stake & Bet Token", "SAB") {
-    whitelist[owner()] = true;
+  constructor() ERC20("Stake & Bet Token", "SAB") {}
+
+  /**
+   * @notice  Set decimals to 6 to match mainstream stablecoins
+   * @return  uint8  .
+   */
+  function decimals() public view virtual override returns (uint8) {
+    return 6;
   }
 
   /**
-   * @notice Checks if the `from` or `to` addresses of the transfer are whitelisted before the transfer is executed
+   * @notice Checks if the `from` or `to` addresses of the transfer are whitelisted/owner before the transfer is executed
+   * @dev This function is called by the transfer and transferFrom functions before the actual transfer is executed. If either address is not whitelisted or the owner, the transfer is reverted.
+   Calling conditions:
+      - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+      will be transferred to `to`.
+      - when `from` is zero, `amount` tokens will be minted for `to`.
+      - when `to` is zero, `amount` of ``from``'s tokens will be burned.
+      - `from` and `to` are never both zero.
    * @param from Address from which the tokens are sent
    * @param to Address to which the tokens are sent
    * @param amount Amount of tokens to be sent
-   * @dev This function is called by the transfer and transferFrom functions before the actual transfer is executed. If either address is not whitelisted, the transfer is not allowed.
    */
   function _beforeTokenTransfer(
     address from,
@@ -28,47 +40,38 @@ contract BetToken is ERC20, Ownable {
     uint256 amount
   ) internal virtual override {
     super._beforeTokenTransfer(from, to, amount);
-    require(
-      whitelist[from] || whitelist[to],
-      "BetToken: Invalid transfer. Only whitelisted _addresses can send/receive tokens."
-    );
+    if (from != address(0) && to != address(0)) {
+      require(
+        isWhitelisted(from) || isWhitelisted(to),
+        "BetToken: Invalid transfer. Only whitelisted addresses or owner can send or receive tokens."
+      );
+    }
   }
 
   /**
    * @notice Adds an address to the whitelist
    * @dev This function can only be called by the owner of the contract
-   * @param _address Address to be added to the whitelist
+   * @param addr Address to be added to the whitelist
    */
-  function addToWhitelist(address _address) public onlyOwner {
-    whitelist[_address] = true;
-    emit WhitelistAdded(_address);
+  function addToWhitelist(address addr) public onlyOwner {
+    whitelist[addr] = true;
+    emit WhitelistAdded(addr);
   }
 
   /**
    * @notice Removes an address from the whitelist
-   * @param _address Address to remove from the whitelist
+   * @param addr Address to remove from the whitelist
    * @dev This function can only be called by the owner of the contract
    */
-  function removeFromWhitelist(address _address) public onlyOwner {
-    whitelist[_address] = false;
-    emit WhitelistRemoved(_address);
-  }
-
-  /**
-   * @notice Change the owner of the contract
-   * @dev Ensure that the old owner is removed from the whitelist and the new owner is added to the whitelist
-   * @param _newOwner The new owner of the contract.
-   */
-  function setNewOwner(address _newOwner) external onlyOwner {
-    removeFromWhitelist(owner());
-    transferOwnership(_newOwner);
-    addToWhitelist(_newOwner);
+  function removeFromWhitelist(address addr) public onlyOwner {
+    whitelist[addr] = false;
+    emit WhitelistRemoved(addr);
   }
 
   /// @notice Checks if an address is whitelisted
-  /// @param _address The address to check
-  /// @return bool Returns true if the address is whitelisted, false otherwise
-  function isWhitelisted(address _address) public view returns (bool) {
-    return whitelist[_address];
+  /// @param addr The address to check
+  /// @return bool Returns true if the address is whitelisted or owner, false otherwise
+  function isWhitelisted(address addr) public view returns (bool) {
+    return whitelist[addr] || addr == owner();
   }
 }
--- test/BetToken.t.sol
+++ test/BetToken.t.sol
@@ -4,31 +4,117 @@ pragma solidity 0.8.16;
 import "forge-std/Test.sol";
 import {BetToken} from "../src/BetToken.sol";
 
+// Derive from original contract to access internal functions (mint) for testing
+contract MintableBetToken is BetToken {
+  function mint(address account, uint256 amount) public {
+    _mint(account, amount);
+  }
+}
+
 contract BetTokenTest is Test {
-  BetToken token;
+  address owner = address(0x1337);
+  address account1 = address(0x1);
+  address account2 = address(0x2);
+
+  uint8 decimals = 6;
+
+  MintableBetToken token;
 
   function setUp() public {
-    token = new BetToken();
+    vm.prank(owner);
+    token = new MintableBetToken();
   }
 
   // -------------------- WHITELIST --------------------
   function test_initialContractState() public {
-    // Check that the owner is whitelisted
-    assertTrue(token.owner() == address(this));
-    assertTrue(token.isWhitelisted(address(this)));
+    assertEq(token.decimals(), decimals);
+    assertEq(token.owner(), owner);
+    assertTrue(token.isWhitelisted(owner));
   }
 
   function test_addToWhitelist() public {
-    address account = address(0x1);
-    token.addToWhitelist(account);
-    assertTrue(token.isWhitelisted(account));
+    vm.prank(owner);
+    token.addToWhitelist(account1);
+
+    assertTrue(token.isWhitelisted(account1));
   }
 
   function test_removeFromWhitelist() public {
-    address account = address(0x1);
-    token.addToWhitelist(account);
-    assertTrue(token.isWhitelisted(account));
-    token.removeFromWhitelist(account);
-    assertTrue(!token.isWhitelisted(account));
+    vm.startPrank(owner);
+    token.addToWhitelist(account1);
+
+    token.removeFromWhitelist(account1);
+    vm.stopPrank();
+
+    assertFalse(token.isWhitelisted(account1));
+  }
+
+  // -------------------- TRANSFERS --------------------
+
+  // Transfer should fail because none of the accounts are whitelisted
+  function test_transferNotWhitelisted() public {
+    _mintTokens(account1, 10000 * 10**decimals);
+    vm.startPrank(account1);
+
+    vm.expectRevert(
+      "BetToken: Invalid transfer. Only whitelisted addresses or owner can send or receive tokens."
+    );
+    token.transfer(account2, 100 * 10**decimals);
+    vm.stopPrank();
+  }
+
+  // Transfer should success because sender is whitelisted
+  function test_transferSenderWhitelisted() public {
+    _mintTokens(account1, 10000 * 10**decimals);
+    vm.prank(owner);
+    token.addToWhitelist(account1);
+
+    vm.prank(account1);
+    token.transfer(account2, 100 * 10**decimals);
+  }
+
+  // Transfer should success because recipient is whitelisted
+  function test_transferRecipientWhitelisted() public {
+    _mintTokens(account1, 10000 * 10**decimals);
+    vm.prank(owner);
+    token.addToWhitelist(account2);
+
+    vm.prank(account1);
+    token.transfer(account2, 100 * 10**decimals);
+  }
+
+  // Transfer should success because both sender and recipient are whitelisted
+  function test_transferBothWhitelisted() public {
+    _mintTokens(account1, 10000 * 10**decimals);
+    _mintTokens(account2, 10000 * 10**decimals);
+    vm.startPrank(owner);
+    token.addToWhitelist(account1);
+    token.addToWhitelist(account2);
+    vm.stopPrank();
+
+    vm.prank(account1);
+    token.transfer(account2, 100 * 10**decimals);
+    vm.prank(account2);
+    token.transfer(account1, 100 * 10**decimals);
+  }
+
+  // -------------------- ADMIN --------------------
+  function test_setNewOwner() public {
+    address oldOwner = owner;
+    address newOwner = address(0x1338);
+    vm.startPrank(owner);
+
+    assert(token.owner() == oldOwner);
+    token.transferOwnership(newOwner);
+    assert(token.owner() == newOwner);
+
+    assertFalse(token.isWhitelisted(oldOwner));
+    assertTrue(token.isWhitelisted(newOwner));
+    vm.stopPrank();
+  }
+
+  // -------------------- HELPERS --------------------
+  function _mintTokens(address account, uint256 amount) private {
+    token.mint(account, amount);
   }
 }
